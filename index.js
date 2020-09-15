bf110e0ce33e0c1b483fe7d16b209e420197a500
Farouk YEDDOU <yedfir@hotmail.fr>
2020-09-15 13:31:57 +0200

    inital setup

---
+++ index.js
@@ -0,0 +1,31 @@
+// import section
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT,() => console.log(`The server is running in ${PORT}`));
+
+// set up mongoose
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,
+    {useNewUrlParser:true,
+    useUnifiedTopology:true},
+    (err) => {
+        if(err) throw err;
+        else console.log("mongoDB connected !")
+    })
+
+
+// set up routes
+
+app.use('/users',require("./routes/userRouter"));
---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// mnogodb collections schema
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 5 },
+  displayName: { type: String },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
